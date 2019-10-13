d693500e078cc41dc91ea2cae2d59b9b2e2445d4
angelito borinaga <borinaga.angelito@gmail.com>
2019-10-13 13:28:34 +0800

    pages data, filters, directives,methods

--- Frontend/src/pages/Index.vue
+++ Frontend/src/pages/Index.vue
@@ -1,11 +1,74 @@
 <template>
-  <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+  <q-page padding>
+    <button class="top"
+    @click="counter++">
+    {{counter}}</button>
+
+    <input v-model="message"
+    @keyup.enter="alertMessage"
+    @keyup.esc="clearMessage"
+    v-autofocus/>
+
+    <button @click="clearMessage">Clear</button>
+
+    <h5 v-if="message.length"
+    class="border-grey" >{{ message }}</h5>
+
+    <h6 v-else>{{noMessage}}</h6>
+
+    <hr>
+
+    <p>UpperCase Message: {{messageUpperCase}}</p>
+
+    <br>
+
+    <p>LowerCase Message: {{message | messageLowerCase}}</p>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  data(){
+    return{
+      message: 'I Love You Marinard Florendo',
+      noMessage: 'No Message Entered',
+      counter: 0
+    } 
+  },
+  computed: {
+    messageUpperCase(){
+      console.log('Message was being fired')
+      return this.message.toUpperCase()
+    },
+  },
+  filters: {
+    messageLowerCase(value){
+      return value.toLowerCase()
+    }
+  },
+  methods: {
+    clearMessage(){
+      this.message = ''
+    },
+    alertMessage(){
+      alert(this.message)
+    },
+  },
+  directives: {
+    autofocus: {
+      inserted(el){
+        el.focus()
+      }
+    }
+  }
 }
 </script>
+<style>
+  .border-grey{
+    border: 15px solid grey;
+  }
+  .top{
+    position: absolute;
+    right: 20px;
+  }
+</style>
